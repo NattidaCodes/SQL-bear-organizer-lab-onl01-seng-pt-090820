8e500762c90b5f7dad2dc6a8e25e5cd83a61902c
Nattida Wong <nattidac.wong@gmail.com>
2020-11-16 12:38:45 -0500

    commit tree e46dc9aa64a503e4eb2ed0831b30782bcd93bcbe

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 98, "M", "dark chocolate", "bitter", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 23, "M", "brown", "wild", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 32, "F", "light brown", "sassy", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 56, "M", "brown", "serious", 0);
+INSERT INTO bears VALUES (5, "Melissa", 45, "F", "white", "cranky", 1);
+INSERT INTO bears VALUES (6, "Grinch", 37, "M", "green", "mean", 1);
+INSERT INTO bears VALUES (7, "Wendy", 20, "F", "yellow", "chill", 1);
+INSERT INTO bears VALUES (8, null, 55, "F", "caramel brown", "sweet", 0);
